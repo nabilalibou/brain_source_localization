82d6c378b1f308deff78faea5285441855e034e6
Nabil-AL <nabil.alibou@gmail.com>
2024-01-27 23:18:32 +0100

    Restructure repo

--- MNE.m
+++
@@ -1,4 +0,0 @@
-function [SOut]=MNE(X,A, lambda);
-
-
-SOut = A'*inv(A*A'+lambda*eye(length(X)))*X;
--- betadist.m
+++
@@ -1,5 +0,0 @@
-function y=betadist(x,a,b)
-% probability density function of the beta distribution with parameters a
-% and b
-
-y=(x.^(a-1).*(1-x).^(b-1))/beta(a,b);
